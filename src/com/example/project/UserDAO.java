6bc57afb852cbd715cb1c9d5a96131a941260966
114252421 <114252421@qq.com>
2024-05-21 14:37:34 +0800

    Add methods insert, delete, update, selectAll, selectByUsername in UserDAO class

---
+++ src/com/example/project/UserDAO.java
@@ -0,0 +1,27 @@
+package com.example.project;
+
+import java.util.List;
+
+public class UserDAO {
+    public void insert(User user) {
+        // implementation here
+    }
+
+    public void delete(int id) {
+        // implementation here
+    }
+
+    public void update(User user) {
+        // implementation here
+    }
+
+    public List<User> selectAll() {
+        // implementation here
+        return null;
+    }
+
+    public User selectByUsername(String username) {
+        // implementation here
+        return null;
+    }
+}
